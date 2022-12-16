a801e2ccb4c1264ad9d00efaca2bd7449afa07ee
unknown <kigucha1234567@gmail.com>
2022-12-16 23:59:02 +0545

    change in route

--- src/app.js
+++ src/app.js
@@ -12,7 +12,9 @@ import vendor_route from "../routes/vendor_route.js";
 const app = express();
 
 app.use(express.json());
-app.use("/","welcome to sawhazDhankuta Api")
+app.use("/",function(req,res){
+  res.status(200).send("Welcome to sawhaaz Dhankuta Api")
+})
 //user API Route
 app.use("/api/user", user_router);
 //category API Route
